d9b0c416672efcff2d13a971d2cbffb8e7caad95
Leandro09 <LuisLeandro09@gmail.com>
2014-07-13 11:15:59 -0600

    Agregar las clases correspondientes.

--- Classes/Player.h
+++ Classes/Player.h
@@ -4,15 +4,19 @@
 #include "cocos2d.h"
 #include "TileMaps.h"
 ///Estructura del player para todo el juego
+
+
 struct Player
 {
-    friend class TileMaps;
-    friend class MercuryScene;
- protected:
-    cocos2d::Sprite* PlayerSprite;
-    int points;
+	friend class TileMaps;
+	friend class MercuryScene;
+protected:
+	cocos2d::Sprite* PlayerSprite;
+	int points = 0;
+	bool colision = false;
 public:
-    Player();
+	Player();
+	~Player();
 
 
 };
